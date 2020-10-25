ef1e44e7b2c6c1ff8c6ba2347bf9a73581957d83
IULIA <iulia.severin@ulbsibiu.ro>
2020-10-25 14:49:08 +0200

    Finish

--- GenerareDateAntrenament-Laborator_1/Spatiu_de_reprezentare_date.Designer.cs
+++ GenerareDateAntrenament-Laborator_1/Spatiu_de_reprezentare_date.Designer.cs
@@ -28,79 +28,21 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.zeroy = new System.Windows.Forms.Label();
-            this.zerox = new System.Windows.Forms.Label();
-            this.treisutey = new System.Windows.Forms.Label();
-            this.treisutex = new System.Windows.Forms.Label();
             this.SuspendLayout();
-            // 
-            // zeroy
-            // 
-            this.zeroy.AutoSize = true;
-            this.zeroy.BackColor = System.Drawing.SystemColors.WindowText;
-            this.zeroy.ForeColor = System.Drawing.SystemColors.HighlightText;
-            this.zeroy.Location = new System.Drawing.Point(231, 427);
-            this.zeroy.Name = "zeroy";
-            this.zeroy.Size = new System.Drawing.Size(16, 17);
-            this.zeroy.TabIndex = 0;
-            this.zeroy.Text = "0";
-            // 
-            // zerox
-            // 
-            this.zerox.AutoSize = true;
-            this.zerox.BackColor = System.Drawing.SystemColors.WindowText;
-            this.zerox.ForeColor = System.Drawing.SystemColors.Window;
-            this.zerox.Location = new System.Drawing.Point(12, 214);
-            this.zerox.Name = "zerox";
-            this.zerox.Size = new System.Drawing.Size(16, 17);
-            this.zerox.TabIndex = 1;
-            this.zerox.Text = "0";
-            // 
-            // treisutey
-            // 
-            this.treisutey.AutoSize = true;
-            this.treisutey.BackColor = System.Drawing.SystemColors.WindowText;
-            this.treisutey.ForeColor = System.Drawing.Color.White;
-            this.treisutey.Location = new System.Drawing.Point(-4, 30);
-            this.treisutey.Name = "treisutey";
-            this.treisutey.Size = new System.Drawing.Size(32, 17);
-            this.treisutey.TabIndex = 2;
-            this.treisutey.Text = "300";
-            // 
-            // treisutex
-            // 
-            this.treisutex.AutoSize = true;
-            this.treisutex.BackColor = System.Drawing.SystemColors.Desktop;
-            this.treisutex.ForeColor = System.Drawing.SystemColors.HighlightText;
-            this.treisutex.Location = new System.Drawing.Point(411, 427);
-            this.treisutex.Name = "treisutex";
-            this.treisutex.Size = new System.Drawing.Size(32, 17);
-            this.treisutex.TabIndex = 3;
-            this.treisutex.Text = "300";
             // 
             // Spatiu_de_reprezentare_date
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(482, 453);
-            this.Controls.Add(this.treisutex);
-            this.Controls.Add(this.treisutey);
-            this.Controls.Add(this.zerox);
-            this.Controls.Add(this.zeroy);
+            this.ClientSize = new System.Drawing.Size(852, 663);
             this.Name = "Spatiu_de_reprezentare_date";
             this.Text = "Generare date de antrenament";
             this.Paint += new System.Windows.Forms.PaintEventHandler(this.canvas_Paint);
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Label zeroy;
-        private System.Windows.Forms.Label zerox;
-        private System.Windows.Forms.Label treisutey;
-        private System.Windows.Forms.Label treisutex;
     }
 }
 
--- GenerareDateAntrenament-Laborator_1/Spatiu_de_reprezentare_date.cs
+++ GenerareDateAntrenament-Laborator_1/Spatiu_de_reprezentare_date.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -34,16 +35,14 @@ namespace GenerareDateAntrenament_Laborator_1
         //initialize zone between [0,3] 
         private double zone;
         //maximum points
-        private int points = 2;
-
+        private int points = 10000;
 
         public Spatiu_de_reprezentare_date()
         {
-            file = new System.IO.StreamWriter("coordinates.txt");
+            file = new StreamWriter("coordinates.txt");
             InitializeComponent();
-            Zone();
         }
-
+        
         //am desenat axele x0y
         private void canvas_Paint(object sender, PaintEventArgs e)
         {
@@ -51,84 +50,106 @@ namespace GenerareDateAntrenament_Laborator_1
             Brush black = new SolidBrush(Color.Black);
             Brush white = new SolidBrush(Color.White);
             Pen whitePen = new Pen(white, 2);
-
-           // gObject.FillRectangle(black, 0, 0, 500, 500);
-            gObject.DrawRectangle(whitePen, 30, 30, 300, 300);
-            gObject.DrawLine(whitePen,180,30,180,330);
-            gObject.DrawLine(whitePen, 30, 180, 330, 180);
+            gObject.FillRectangle(black, 0, 0, 640, 540);
+            gObject.DrawRectangle(whitePen, 20, 20, 600, 500);
+            gObject.DrawLine(whitePen,320,20,320,520);
+            gObject.DrawLine(whitePen, 20, 270, 620, 270);
             gObject.Dispose();
-          
+            Zone();
         }
         private void Zone()
         {
-            float prag;
+            double prag;
             double G;
 
             for (int i = 0; i < points; i++)
             {
                 zone = random.Next(0, 3);
+               /* prag = (double)(random.Next(0, 10000) / 10000.0);*/
                 do
                 {
                     x = RandomNumber();
+                   
+                   
                     //random.Next() - returns a positive random integer that is less than the specified maximum.
-                    prag = (float)(random.Next(10000) / 10000);
+                    prag = (double)(random.Next(0, 1000) / 1000.0);
+
                     if (zone == 0)
                     {
                         G = GaussFunction(x, mx1, sigmax1);
+                     //   Console.WriteLine("G:"+G+" "+"prag"+ prag+"zona "+ zone);
                     }
                     else if (zone == 1)
                     {
                         G = GaussFunction(x, mx2, sigmax2);
+                      //  Console.WriteLine("G:" + G + " " + "prag" + prag + "zona " + zone);
                     }
                     else
                     {
                         G = GaussFunction(x, mx3, sigmax3);
+                      //  Console.WriteLine("G:" + G + " " + "prag" + prag + "zona " + zone);
                     }
                 } while (G < prag);
 
                 do
                 {
                     y = RandomNumber();
+                    
                     //random.Next() - returns a positive random integer that is less than the specified maximum.
-                    prag = (float)(random.Next(10000) / 10000);
+                    //prag = (double)(random.Next(0, 10000) / 10000.0);
+                    prag = (double)(random.Next(0, 1000) / 1000.0);
+
                     if (zone == 0)
                     {
                         G = GaussFunction(y, my1, sigmay1);
+                       // Console.WriteLine("G:" + G + " " + "prag" + prag + "zona " + zone);
                     }
                     else if (zone == 1)
                     {
                         G = GaussFunction(y, my2, sigmay2);
+                      //  Console.WriteLine("G:" + G + " " + "prag" + prag + "zona " + zone);
                     }
                     else
                     {
                         G = GaussFunction(y, my3, sigmay3);
+                       // Console.WriteLine("G:" + G + " " + "prag" + prag + "zona " + zone);
                     }
                 } while (G < prag);
 
-                draw(x, y);
-                file.WriteLine(x + " " + y + " " + zone);
+                //G=0 si prag foarte mic -> am puncte in stanga jos -clopotul ar veni pe 0 acolo jos
+                Draw(x, y);
+                //Draw(x+300, 300-y);
+              //  file.WriteLine(x + " " + y + " " + zone);
+                file.WriteLine("Coordonates: " + "x=" + x.ToString() + " y=" + y.ToString() + " zona=" + zone.ToString());
             }
-
+            //file.Dispose();
         }
 
-        public void draw(double x, double y)
+        private void Draw(double x, double y)
         {
-            Graphics gObject = CreateGraphics();
 
+            Graphics gObject = CreateGraphics();
+            //x = Math.Abs(x);
+            //y = Math.Abs(y);
+            /*    Console.Write(x + " " + y + " " + "\t");*/
+            x = x+300;
+            y = 300- y;
+            /*    Console.WriteLine(x + " " + y + " " + zone);*/
             if (zone == 0)
             {
-                gObject.FillEllipse(new SolidBrush(Color.Red), (float)x, (float)y, 2.0f, 2.0f);
+                gObject.FillEllipse(new SolidBrush(Color.Red), (float)x, (float)y, 2.0F, 2.0F);
             }
             else if (zone==1)
             {
-                gObject.FillEllipse(new SolidBrush(Color.BlueViolet), (float)x, (float)y, 2.0f, 2.0f);
+                gObject.FillEllipse(new SolidBrush(Color.BlueViolet), (float)x, (float)y, 2.0F, 2.0F);
             }
             else
             {
-                gObject.FillEllipse(new SolidBrush(Color.YellowGreen), (float)x, (float)y, 2.0f, 2.0f);
+                gObject.FillEllipse(new SolidBrush(Color.YellowGreen), (float)x, (float)y, 2.0F, 2.0F);
             }
-        }
 
+            gObject.Dispose();
+        }
         //random.NextDouble returns a double between 0 and 1. Multipling that by 
         //the range the number should be into and add that to the base (minimum)
         private double RandomNumber()
