1b844065f633a3d9c30e4ce4f20e197f0a4d7553
IULIA <iulia.severin@ulbsibiu.ro>
2020-11-21 21:36:23 +0200

    Generare Centroizi Random

--- KMeans-Laborator_2/Spatiu_de_reprezentare_date.cs
+++ KMeans-Laborator_2/Spatiu_de_reprezentare_date.cs
@@ -12,6 +12,16 @@ namespace KMeans_Laborator_2
 {
     public partial class Spatiu_de_reprezentare_date : Form
     {
+
+        Random random = new Random();
+        private int k, newX, newY;
+        private List<Point> points = new List<Point>();
+        private List<Centroid> centroizi = new List<Centroid>();
+        private List<Color> oldColor = new List<Color>();
+        private Color newColor;
+        private Color[] colors = new Color[] { Color.Magenta, Color.LightBlue, Color.LightCoral, Color.LightGreen, Color.Orange, Color.Orchid, Color.Olive, Color.Pink, Color.Purple, Color.PowderBlue };
+        private object panelWindow;
+
         public Spatiu_de_reprezentare_date()
         {
             InitializeComponent();
@@ -29,11 +39,61 @@ namespace KMeans_Laborator_2
             gObject.DrawLine(whitePen, 320, 20, 320, 520);
             gObject.DrawLine(whitePen, 20, 270, 620, 270);
             gObject.Dispose();
-           // Zone();
+            GenerareCentroizi();
+            DrawCentroizi();
         }
 
+        //generez centroizii
+        private void GenerareCentroizi()
+        {
+            // k = random.Next(2, 10);
+            k = 5;
+            for (int i = 0; i < k; i++)
+            {
+                newX = RandomNumber();
+                newY = RandomNumber();
 
+                //cat timp noua culoarea nu a mai fost folosita, alege o noua culoare
+                do
+                {
+                    newColor = colors.ElementAt(random.Next(1, 10));
+                }
+                while (oldColor.Contains(newColor));
+                oldColor.Add(newColor);
+                centroizi.Add(new Centroid(newX, newY, newColor, i + 1));
 
+            }
+            Console.WriteLine(k);
+        }
+        private void DrawCentroizi()
+        {
+            Graphics gObject = CreateGraphics();
+
+            foreach (Centroid c in centroizi)
+            {
+                int xCentroid = 300 + c.getX();
+                int yCentroid = 300 - c.getY();
+
+                //  int xCentroid = panelWindow.Width - panelWindow.Width / 2 + c.getX();
+                // int yCentroid = panelWindow.Height - panelWindow.Height / 2 - c.getY();
+                if ((xCentroid < 600 && xCentroid > 2) && (yCentroid < 577 && yCentroid > 2))
+                {
+                    
+                    gObject.FillEllipse(new SolidBrush(c.color), new Rectangle(xCentroid - 5, yCentroid - 5, 10, 10));
+                   
+                }
+             //   Console.WriteLine(xCentroid + " " + yCentroid);
+            }
+            gObject.Dispose();
+
+        }
+
+        private int RandomNumber()
+        {
+            int min = -300;
+            int max = 300;
+            return random.Next(min, max);
+        }
 
 
 
